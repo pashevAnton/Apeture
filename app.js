c2a39f90fb51880e32dab72f354798266dd1a393
PagilayaSkumbria54 <toni.pashev@bk.ru>
2022-10-31 21:08:30 +0300

    Clone repository

--- app.js
+++ app.js
@@ -1,4 +1,40 @@
-window.onload = function(){
+const hamburger = document.querySelector('.hamburger')
+const linesHamburger = document.querySelectorAll('.line')
+const headerMenu = document.querySelector('.menu_top')
+new WOW().init();
 
-    window.scrollTo(0, 999999999999999);
+hamburger.addEventListener('click', (e) => {
+    e.stopPropagation()
+    for (const line of linesHamburger){
+        line.classList.toggle('is-active')
+    }
+    hamburger.classList.toggle('is-active')
+    headerMenu.classList.toggle('is-active')
+})
+
+document.onclick = function() {
+    for (const line of linesHamburger){
+        line.classList.remove('is-active')
+    }
+    hamburger.classList.remove("is-active");
+    headerMenu.classList.remove("is-active");
 }
+
+
+
+$('a.anchor').on('click', function(event) {
+    var $anchor = $(this);
+    $('html, body').stop().animate({
+        scrollTop: $($anchor.attr('href')).offset().top - 57}, {
+        duration: 1200,
+        specialEasing: {
+            width: "linear",
+            heigth: "easeInOutCcubic"
+        }
+    });
+    event.preventDefault();
+});
+
+
+
+
